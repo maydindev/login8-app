44d3a30f31555bc12e8088e568dfdb361468a6c6
maydindev <maydin.dev@outlook.com>
2024-10-03 09:50:34 +0300

    "Updated globals.css to change font family, modified layout.js to update metadata title, and rewritten page.js to replace existing content with a new login page design."

--- src/app/page.js
+++ src/app/page.js
@@ -2,100 +2,100 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="relative flex flex-col justify-center items-center min-h-screen">
+      <svg
+        className="absolute inset-0 left-[77%] z-10"
+        width="345"
+        height="618"
+        viewBox="0 0 345 618"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M819 41C819 -106.484 210.881 -60.8184 66.5343 -60.8184C-77.8128 -60.8184 454.414 553.099 66.5343 617.503C230.584 562.366 -199.488 188.503 125 258.003C449.488 327.503 84.8411 96.0245 819 41Z"
+          fill="url(#paint0_linear_0_25)"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <defs>
+          <linearGradient
+            id="paint0_linear_0_25"
+            x1="36.6371"
+            y1="493.94"
+            x2="36.6371"
+            y2="-153.48"
+            gradientUnits="userSpaceOnUse"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            <stop offset="0.411458" stopColor="#02B94B" stopOpacity="0.84" />
+            <stop offset="1" stopColor="#046AB4" stopOpacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <svg
+        className="absolute inset-0 top-[23%] z-10"
+        width="709"
+        height="582"
+        viewBox="0 0 709 582"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M709 582C709 741.155 128.904 623.5 -11 623.5C-150.904 623.5 -239.44 516.655 -239.44 357.5C-80.4399 417 -311.44 82.0001 3.06012 7.00012C317.56 -67.9999 -2.56012 522.621 709 582Z"
+          fill="url(#paint0_linear_0_24)"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_0_24"
+            x1="234.78"
+            y1="0.429901"
+            x2="234.78"
+            y2="699.083"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0.411458" stopColor="#02B94B" stopOpacity="0.84" />
+            <stop offset="1" stopColor="#046AB4" stopOpacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <div className="relative z-20 flex flex-col justify-start shadow-2xl p-7  bg-opacity-50 rounded-3xl ">
+        <h1 className="text-[#02B94B] text-5xl font-extrabold text-start mb-7 w-[256]">
+          LOGIN
+        </h1>
+        <input
+          type="text"
+          placeholder="Username"
+          className="w-[350px] h-12 p-5 text-xl mb-5 rounded-lg border border-gray-400 placeholder-gray-600"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="w-[350px] h-12 p-5 text-xl mb-5 rounded-lg border border-gray-400 placeholder-gray-600"
+        />
+        <div className="mb-10">
+          <button className="bg-[#02B94B] bg-opacity-70 text-white text-xl w-36 h-10 rounded-lg font-bold">
+            LOGIN
+          </button>
+        </div>
+        <div className="flex items-center mb-7">
+          <div className="flex-1 border-t border-black"></div>
+          <span className="mx-4 font-bold text-black">OR</span>
+          <div className="flex-1 border-t border-black"></div>
+        </div>
+        <div className="flex justify-center items-center">
+          <a href="">
+            <img src="/google.png" alt="" className="w-6 h-6 mr-7" />
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+          <a href="">
+            <img src="/facebook.png" alt="" className="w-6 h-6 ml-7" />
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <br />
+        <div>
+          <div className="flex justify-between mx-[-20px] mb-[-20px]  text-sm  text-[#02B94B]">
+            <a href="">Forgot password</a>
+            <a href="">Register</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
